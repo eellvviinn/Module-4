31a9a4b79628b8ace52e3ca02e181ba40e1e6294
cloudate8 <144731911+eellvviinn@users.noreply.github.com>
2024-01-25 12:46:01 -0800

    Initial commit

---
+++ src/com/skymate8/module4/one/CSVUtils.java
@@ -0,0 +1,40 @@
+package com.skymate8.module4.one;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVUtils {
+    public static void createCSVFile(String filePath, List<List<String>> content) throws IOException {
+        StringBuilder toBeWritten = new StringBuilder();
+        for (List<String> row : content) {
+            StringBuilder sb = new StringBuilder();
+            for (String cell : row) {
+                sb.append(cell).append(",");
+            }
+            sb.replace(sb.length() - 1, sb.length(), "\n");
+            toBeWritten.append(sb);
+        }
+        toBeWritten.delete(toBeWritten.length() - 1, toBeWritten.length());
+        Files.writeString(Path.of(filePath), toBeWritten);
+    }
+    public static List<List<String>> getCSVFileContent(String filePath) throws IOException {
+        List<List<String>> content = new ArrayList<>();
+        for (String column : Files.readAllLines(Path.of(filePath))) {
+            List<String> row = List.of(column.split(","));
+            content.add(row);
+        }
+        return content;
+    }
+    public static List<String> getRow(String filePath, int rowNumber) throws IOException {
+        List<List<String>> content = getCSVFileContent(filePath);
+        List<String> row = new ArrayList<>();
+        for (int i = 1; i < content.size(); i++) {
+            List<String> column = content.get(i);
+            row.add(column.get(rowNumber));
+        }
+        return row;
+    }
+}
